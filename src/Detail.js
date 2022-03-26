76d50112af1436876741fe27d0c313ff5d31fa3c
crown0205 <crown0205@gmail.com>
2022-03-26 14:34:46 +0900

    3-12 delete 버튼 기능 구현

--- src/Detail.js
+++ src/Detail.js
@@ -1,14 +1,30 @@
 // 리액트 패키지를 불러옵니다.
 import React from "react";
-import { useParams } from "react-router-dom"
-import { useSelector } from "react-redux"
+import { useParams, useHistory } from "react-router-dom"
+import { useSelector, useDispatch } from "react-redux"
+import { deleteBucket } from "./redux/modules/bucket";
 
 const Detail = (props) => {
+  const dispatch = useDispatch()
   const pramas = useParams();
+  const history = useHistory()
   const bucket_list = useSelector((state)=> state.bucket.list)
+  const bucket_index = pramas.index // item의 index
   const bucket_text = bucket_list[pramas.index]
 
-  return <h1>{bucket_text}</h1>;
+  return (
+    <>
+    <h1>{bucket_text}</h1>
+    <button onClick={()=>{
+      console.log('삭제 버튼 눌림')
+      // history.goBack();
+      dispatch(deleteBucket(bucket_index)) // 4) 버튼에 dispatch를 통해 액션 함수 실행, 
+    }}>                                   {/*  그리고 item의 몇번째인지 알아야하기 때문에 index를 같이 넘겨서 redux로 보내준다 */}
+
+      삭제하기
+    </button>
+    </>
+  );
 };
 
 export default Detail;
--- src/redux/modules/bucket.js
+++ src/redux/modules/bucket.js
@@ -1,8 +1,9 @@
-// ㅠㅕ찯ㅅ.js
+// bucket.js
 
 // Actions
 // const LOAD = "my-app/widgets/LOAD"; // 서버에서 가져올때 쓰는거
 const CREATE = "bucket/CREATE";
+const DELETE = "bucket/DELETE"; // 1) 먼저 타입부터 선언해준다.
 
 const initState = {
   list : [
@@ -18,6 +19,12 @@ export function cerateBucket(bucket){
   return {type: CREATE, bucket: bucket}  // bucket값이 새로운 데이터다 그게 action으로 받아진다.
 }                                        // "action.bucket" 25번째열
 
+export function deleteBucket(bucket_index){   // 2) 액션 타입은 선언 했으니 액션을 만들어주자.
+  // console.log('DELETE 액션 실행함')
+  console.log('지울 인덱스', bucket_index)
+  return {type: DELETE, bucket_index} 
+}                                        
+
 // Reducer                      // 기본값 <= 7번째열  // action에 값이 없으면 빈배열이 들어간다라는 의미.
 export default function reducer(state = initState, action = {}) {
   switch (action.type) {
@@ -27,6 +34,18 @@ export default function reducer(state = initState, action = {}) {
       const new_bucket_list = [...state.list, action.bucket]; // 그게 새데이터인 "bucket: bucket"가 기존 배열을 복사한거에다가 새로운 데이터를 받아 추가하는거다. 
       return {list: new_bucket_list};                         // action으로 받아진다.
     }
+    case "bucket/DELETE" : {           // 3) 액션을 실행해서 어떻게 바꿔줄지 실체적으로 일하는곳.
+      console.log("state",state)
+      console.log("action",action)
+      const new_bucket_list = state.list.filter((l, index)=>{
+        // console.log(parseInt(action.bucket_index) !== index, parseInt(action.bucket_index), index) 
+        // filter을 이용해 같은 값을 가진 item만 걸러내고 나머지를 반환하는 방법을 사용함.
+        return parseInt(action.bucket_index) !== index;
+      })
+      console.log(new_bucket_list)  // 최종확인 
+      console.log({list: new_bucket_list})  // 최종 반환 할떄는는 initState의 형식대로 반환해줘야 에러가 안난다.
+      return {list : new_bucket_list};
+    } 
     default:
       return state;
   }
