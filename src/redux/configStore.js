45fa7ee7066243acd1bc74eea02aee4573a7455d
crown0205 <crown0205@gmail.com>
2022-03-26 21:32:47 +0900

    파이어베이스에서 데이터를 가져와 사용하기 위해서, thunk 패키지를 설치해 미들웨어를 스토어에 적용

--- src/redux/configStore.js
+++ src/redux/configStore.js
@@ -1,9 +1,15 @@
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+
 import bucket from "./modules/bucket";
 
-const rootReducer = combineReducers({ bucket }); // 여기에 리듀서가 더 생길시 "bucket" 자리에 더 추가 해주면 리듀서를 묶을수 있다.
-                                                 // "bucket"명이 데이터 값 앞에 붙어서 나옴.
-                                                 // bucket : list (3) 이렇게 콘솔창에 initState 값이 들어가 있음.
-const store = createStore(rootReducer);
+const middlewares = [thunk] // 쓸 미들웨어를 넣어주면 된다.
+const rootReducer = combineReducers({ bucket });
+const enhancer = applyMiddleware(...middlewares) //미들웨어를 묶어주는 애.(애들의 모음)
+// const enhancer = applyMiddleware(thunk, thunk2, ...) // 더 많으면 이런식으로 작성해준다
+
+
+const store = createStore(rootReducer, enhancer); // 스토어에 미들웨어 적용하는거 끝~!
+// 스토어를 만들때, 리듀서와 욥셔널한걸 엮어서 만든다. "⬆️"
 
 export default store;
